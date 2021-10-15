074c1e936f9d1ab2942e50d992bd90e64ea7a024
ultraaverina <super.star1985@mail.ru>
2021-10-15 14:47:50 +0500

    final

--- Api/Controllers/CabinetController.cs
+++ Api/Controllers/CabinetController.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Api.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,37 @@ namespace Api.Controllers
     [Authorize]
     public class CabinetController : Controller
     {
+        //private UserManager<IdentityUser> _userManager;
+        //private readonly TestTaskContext _context;
+        //public CabinetController(TestTaskContext context,UserManager<IdentityUser> manager)
+        //{
+        //    _context = context;
+        //    _userManager = manager;
+        //}
+
+      
         public IActionResult Index()
         {
-            return View();
+            
+
+          return View();
+         
+        }
+        //public async void GetUserInfo()
+        //{
+        //    var user = await _userManager.GetUserAsync(User);
+        //    var email = user.Email;
+        //    var phone = user.PhoneNumber;
+        //    var FIO = user.NormalizedUserName;
+        
+
+        //}
+
+
+
+        public IActionResult Exit(){ 
+            
+            return RedirectToAction("Login","Account");
         }
     }
 }
